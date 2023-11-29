58bc9f6280d16d064ceb26e5a3ca0957f69e9127
Seio924 <134473627+Seio924@users.noreply.github.com>
2023-11-29 21:46:32 +0900

    코드 정

--- hw3/q1.py
+++ hw3/q1.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-def draw_histo(hist, shape=(200, 256)):
+def draw_histo(hist, shape):
     hist_img = np.full(shape, 0, np.uint8)  # 검은색 배경으로 초기화
     cv2.normalize(hist, hist, 0, shape[0], cv2.NORM_MINMAX)
     gap = hist_img.shape[1] / hist.shape[0]
@@ -15,27 +15,21 @@ def draw_histo(hist, shape=(200, 256)):
 
     return cv2.flip(hist_img, 0)
 
-image = cv2.imread("img/affine_test1.jpg", cv2.IMREAD_GRAYSCALE)
+image = cv2.imread("img/image1.jpg", cv2.IMREAD_GRAYSCALE)
 if image is None:
     raise Exception("영상파일 읽기 오류")
 
-# 수직 방향 투영 (열 방향 합)
 vertical_projection = cv2.reduce(image, 0, cv2.REDUCE_AVG).ravel().astype(int)
 
-# 수평 방향 투영 (행 방향 합)
 horizontal_projection = cv2.reduce(image, 1, cv2.REDUCE_AVG).ravel().astype(int)
 
+hist_img1 = draw_histo(vertical_projection, (image.shape[0], image.shape[1]))
 
-hist_img1 = draw_histo(vertical_projection)
-hist_img2 = draw_histo(horizontal_projection)
+hist_img2 = draw_histo(horizontal_projection, (image.shape[1], image.shape[0]))
 
 hist_img2 = cv2.rotate(hist_img2, cv2.ROTATE_90_CLOCKWISE)
 
-hist_img1 = cv2.resize(hist_img1, (image.shape[1], image.shape[0]))
-hist_img2 = cv2.resize(hist_img2, (image.shape[1], image.shape[0]))
-
-
 cv2.imshow("image", image)
-cv2.imshow("Vertical Projection", hist_img1)
-cv2.imshow("Horizontal Projection", hist_img2)
+cv2.imshow("hist_ver", hist_img1)
+cv2.imshow("hist_hor", hist_img2)
 cv2.waitKey(0)
--- hw3/q2.py
+++ hw3/q2.py
@@ -1,24 +1,25 @@
-import numpy as np, cv2
+import numpy as np, cv2, time
 
-image_original = cv2.imread("img/color_edge.jpg", cv2.IMREAD_COLOR)
+image = cv2.imread("img/test.jpg", cv2.IMREAD_COLOR)
+start = time.time()
 
-R, G, B = cv2.split(image_original)
-
-Y = 0.299 * R + 0.587 * G + 0.114 * B
-Cb = (R - Y) * 0.564 + 128
-Cr = (B - Y) * 0.713 + 128
-
+R, G, B = cv2.split(image)
+Y = np.clip(0.299 * R + 0.587 * G + 0.114 * B, 0, 255)
+Cb = np.clip((B - Y) * 0.564 + 128, 0, 255)
+Cr = np.clip((R - Y) * 0.713 + 128, 0, 255)
 YCbCr = cv2.merge([Y, Cb, Cr])
+
 Y, Cb, Cr = cv2.split(YCbCr)
+R = np.clip(Y + 1.403 * (Cr - 128), 0, 255)
+G = np.clip(Y - 0.714 * (Cr - 128) - 0.344 * (Cb - 128), 0, 255)
+B = np.clip(Y + 1.773 * (Cb - 128), 0, 255)
+image_revised = cv2.merge([R, G, B])
 
-R = Y + 1.403 * (Cr - 128)
-G = Y - 0.714 * (Cr - 128) - 0.344 * (Cb - 128)
-B = Y + 1.773 * (Cb - 128)
+end = time.time()
+print(f"{end - start:.5f} sec")
 
-image_revised = cv2.merge([B, G, R])
-
-title1 = 'Q13_original'
-title2 = "Q13_revised"
-cv2.imshow(title1, image_original)
-cv2.imshow(title2, cv2.convertScaleAbs(image_revised))
+cv2.imshow("original", image)
+cv2.imshow("YCbCr", cv2.convertScaleAbs(YCbCr))
+cv2.imshow("revised", cv2.convertScaleAbs(image_revised))
 cv2.waitKey(0)
+cv2.destroyAllWindows()
